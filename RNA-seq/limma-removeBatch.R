58fde68ad552bd7aa85ac1d6f25e8cdba86c5427
zxwang-cloak <77724288+zxwang-cloak@users.noreply.github.com>
2023-03-06 18:22:05 +0800

    Update limma-removeBatch.R

--- RNA-seq/limma-removeBatch.R
+++ RNA-seq/limma-removeBatch.R
@@ -2,12 +2,14 @@ library(limma)
 library(FactoMineR)
 library(factoextra)
 library(patchwork)
+
 ########
 tpm<-read.table("for-normalized-four-cohort-tpm.txt", header = T, row.names = 1, sep = "\t")
 batch <- c(rep('CAGA',48),rep('CPGEA',134),rep('TCGA',497),rep('WCDT',99))
 batch <- as.factor(batch)
 design <- model.matrix(~0 + batch)
 ########
+
 condition <- data.frame(sample = colnames(tpm),
                         group = c(rep('CAGA',48),rep('CPGEA',134),rep('TCGA',497),rep('WCDT',99)),
                         batch = c(rep('batch1',48),rep('batch2',134),rep('batch3',497),rep('batch4',99)))
@@ -21,10 +23,12 @@ pre<-fviz_pca_ind(pre.pca,
                   legend.title="Group") + theme_bw() + 
   ggtitle("All gene\nPCA (before batch correction)") +
   theme(plot.title = element_text(face = "bold", hjust = 0.5))
+
 ########
-tpm=normalizeBetweenArrays(tpm)#如果组内的中位数不在同一条水平线上，现在该参数校正，再使用下面的批次校正
+tpm=normalizeBetweenArrays(tpm)
 new_tpm <- removeBatchEffect(tpm, batch = batch)
 ########
+
 #log_new_tpm<-log10(new_tpm+1)
 limma.pca <- PCA(t(new_tpm),graph = FALSE)
 limma<-fviz_pca_ind(limma.pca,
